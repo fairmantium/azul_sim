cb075ac4cc4a5cebdbc6755005f7afa47b523344
Jim Fairman <fairman.jim@gmail.com>
2020-01-20 02:27:31 -0800

    Update azul_slots.R

--- azul_slots.R
+++ azul_slots.R
@@ -7,17 +7,16 @@ setClass("slot", slots = list(slot_num = "numeric",
 # Function To Create an S4 Slot Object
 create_s4_slot <- function(num) {
   
-  values <- new("slot", slot_num = num)
+  slot <- new("slot",
+                slot_num = num
+                )
   
-  return(values)
+  return(slot)
   
 }
 
-# Source Tile
-source("azure_tiles.R")
-
 # Create an Empty S4 Slot
 s <- create_s4_slot(num = 1)
-t <- create_s4_tile(col = "black", num = 1)
+t <- create_s4_tile(col = "black", col_num = 1, num = 1)
 # Place Tile Into Slot
 s@tile <- t
