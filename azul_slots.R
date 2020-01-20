5e035701d31266bbed59918ebc2b76ea30c83d74
Jim Fairman <fairman.jim@gmail.com>
2020-01-20 01:41:00 -0800

    Update to S4 objects.

--- azul_slots.R
+++ azul_slots.R
@@ -1,18 +1,3 @@
-# Create an S3 Object Slot Class
-create_s3_slot <- function(num) {
-  
-  values <- list(slot_num = num,
-                 tile = "tile"
-                 )
-  
-  attr(values, "class") <- "slot"
-  
-  return(values)
-  
-}
-
-
-
 # Define an S4 Object Slot Class
 setClass("slot", slots = list(slot_num = "numeric",
                               tile = "tile"
@@ -31,14 +16,8 @@ create_s4_slot <- function(num) {
 # Source Tile
 source("azure_tiles.R")
 
-# Create an Empty S3 Slot
-s3 <- create_s3_slot(num = 1)
-t3 <- create_s3_tile(col = "blue", num = 10)
-# Place Tile Into Slot
-s3$tile <- t3
-
 # Create an Empty S4 Slot
-s4 <- create_s4_slot(num = 1)
-t4 <- create_s4_tile(col = "black", num = 1)
+s <- create_s4_slot(num = 1)
+t <- create_s4_tile(col = "black", num = 1)
 # Place Tile Into Slot
-s4@tile <- t4
+s@tile <- t
